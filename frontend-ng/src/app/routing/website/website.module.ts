218eacd77c75320b710821e03c0176aaa0cecb83
maxgotech <masjanja24122002@mail.ru>
2023-04-22 16:41:47 +0300

    404 страница и материал ио

--- frontend-ng/src/app/routing/website/website.module.ts
+++ frontend-ng/src/app/routing/website/website.module.ts
@@ -1,12 +1,35 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+    {
+      path: '',
+      loadChildren: () => import('./routing/home/home.module') 
+      .then(module=> module.HomeModule)
+    },
 
+    {
+      path: 'not-found',
+      loadChildren: () => import('./routing/not-found/not-found.module') 
+      .then(module=> module.NotFoundModule)
+    },
+
+    {
+      path: '**',
+      redirectTo:'not-found'
+    }
+
+  ]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations:[],
+  imports:[
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:[RouterModule],
+  providers:[]
+  
 })
 export class WebsiteModule { }
